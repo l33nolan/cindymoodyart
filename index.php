9c2f265f877e3e4e49816fae907625003de0673a
lee nolan <leenolan@lees-MacBook-Air.local>
2018-02-07 18:26:07 +0000

    first deploy

---
+++ index.php
@@ -0,0 +1,315 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <meta name="description" content="">
+    <meta name="author" content="Cindy Moody">
+    <title>Cindy Moody - Hull Artist</title>
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <!-- Custom styles -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body id="page-top">
+
+<div class="wrapper">
+<!-- Navigation -->
+<nav class="navbar navbar-default">
+    <div class="container">
+        <div class="navbar-brand">Cindy Moody</div>
+
+        <!-- Brand and toggle grouped for mobile display -->
+        <div class="navbar-header page-scroll">
+            <button type="button" class="navbar-toggle" data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li class="hidden">
+                    <a href="#page-top"></a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="#portfolio">Portfolio</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="#about">Bio</a>
+                </li>
+
+                <li>
+                    <a class="page-scroll" href="#inspiration">Inspiration</a>
+                </li>
+
+                <li>
+                    <a class="page-scroll" href="#contact">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container-fluid -->
+</nav>
+
+<!--Intro section-->
+<section id="intro" class="c-module">
+    <div class="container intro">
+
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <div class="c-intro-container">
+                    <p>I'm Cindy, an artist/painter based in Hull, East Riding of Yorkshire. I really appreciate you
+                        visiting my site and finding out a bit more about me, and taking a look at some of my work. I
+                        love to paint, and I hope you enjoy my works. Please feel free to get in touch if you have any
+                        feedback, or might be interested in owning a piece for yourself.</p>
+                </div>
+
+            </div>
+
+            <div class="col-md-12 mob-text-center">
+                <div class="mob-intro-container">
+                    <p>I'm Cindy, an artist/painter based in Hull, East Riding of Yorkshire. I really appreciate you
+                        visiting my site and finding out a bit more about me, and taking a look at some of my work.</p>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!-- /.container -->
+</section>
+
+<!--Portfolio Section-->
+<section id="portfolio">
+    <div class="container folio">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <div class="section-title">
+                    <h2>My Work</h2>
+                    <p>I hope that you enjoy my work as much as I enjoy making it. I welcome any comments and of course
+                        if you would like to purchase any pieces for yourself or your loved ones, do not hesitate to
+                        contact me for further information.</p>
+                    <p>Hover or click over an image for more details.</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <div class="row row-0-gutter img-place">
+
+    </div>
+</section>
+
+<!--About/Bio Section-->
+<section id="about" class="c-module">
+    <div class="container bio">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <div class="section-title">
+                    <h2>BIO</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6 text-center">
+                <div class="c-about-container">
+                    <p>I have been interested in the art of drawing and replication from a very early age. As a child I
+                        enjoyed doodling small sketches for family and friends. As a young woman, a distinct lack of
+                        opportunity left me wanting to discover more of the world of art and I held a strong personal
+                        desire to develop my inner creativity. </p>
+                    <p>Saltburn, is my most favourite seaside location of the English coastline, and an absolute delight
+                        for an artist. Much of my inspiration comes from this area. In contrast to the natural beauty of
+                        the sea, I am also intrigued by the naturally occurring urban art that can be found down
+                        alleyways and streets in many cities and towns. Particularly poster art and urban decay. Living
+                        in the city of Hull, I was lucky enough to study at the Hull School of Art and Design. During my
+                        studies I was able to explore the use of various mediums including photography, film and
+                        illustration.</p>
+                </div>
+            </div>
+
+            <div class="col-md-6 text-center">
+                <div class="c-about-container">
+                    <p>I mostly paint in acrylic, due to their colour vibrancy and ease of use, I prefer a quicker
+                        drying medium, although I never shy away from other forms of mark making. I take inspiration
+                        from the world around me, I am interested in nature, objects and anything that visually
+                        stimulates me. I always search for the beauty in any situation, I often travel and always keep a
+                        look out for inspiration. </p>
+                    <p>Relaxing seascapes draw me in and I love to explore the way that the elements unite to create
+                        interesting and exciting forms, particularly the way that water reflects on the body. I am
+                        always finding ways to return to my painting; working and travelling take up my time, but
+                        immersion into the canvas is essential to feed my soul and express my inner creativity.</p>
+                </div>
+            </div>
+
+        </div>
+        <div class="row row-0-gutter">
+
+
+        </div>
+    </div>
+</section>
+
+
+<section id="inspiration">
+    <div class="container inspiration">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <div class="section-title">
+                    <h2>My Inspiration</h2>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+
+            <!-- inspiration item -->
+            <div class="col-lg-4">
+                <div class="insp-item">
+                    <div class="insp-image">
+                        <img src="img/frida.jpg" class="insp-img img-responsive" alt="author">
+                    </div>
+                    <div class="insp-text">
+                        <h3>FRIDA KAHLO</h3>
+                        <p>Frida Kahlo de Rivera was a Mexican painter, who painted many self-portraits. Inspired by Mexican
+                            popular culture, she employed a naïve folk art style to explore questions of identity,
+                            postcolonialism, gender, class, and race in Mexican society. </p>
+                    </div>
+                </div>
+            </div>
+            <!-- end inspiration item -->
+
+            <!-- inspiration item -->
+            <div class="col-lg-4">
+                <div class="insp-item">
+                    <div class="insp-image">
+                        <img src="img/bill.jpg" class="insp-img img-responsive" alt="author">
+                    </div>
+                    <div class="insp-text">
+                        <h3>BILL BATE</h3>
+                        <p>Bill Bate studied at the Central School of Art London, gaining a Fine Art Degree. Since then, He has been painting
+                            for a number of years and during that time has developed a striking and dramatic style based on the human form.
+
+
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <!-- end inspiration item -->
+
+            <!-- inspiration item -->
+            <div class="col-lg-4">
+                <div class="insp-item">
+                    <div class="insp-image">
+                        <img src="img/rachel.jpg" class="insp-img img-responsive" alt="author">
+                    </div>
+                    <div class="insp-text">
+                        <h3>RACHEL WHITEREAD</h3>
+                        <p>Rachel Whiteread is an English artist, primarily a sculptor, typically taking the form of
+                            casts. She was the first woman to win the annual Turner Prize in 1993, and was among the British
+                            Artists who exhibited at the Royal Academy's Sensation exhibition in 1997. </p>
+                    </div>
+                </div>
+            </div>
+            <!-- end inspiration item -->
+
+        </div>
+    </div>
+</section>
+
+<section id="contact">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <div class="section-title">
+                    <h2>Contact Me</h2>
+                    <p>I would love to hear from anybody regarding my artwork. If you have any feedback, questions, or
+                        might be interested in buying a painting, please get in touch!</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <form name="sendMessage" id="contactForm" action="form.php" method="post">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <input name="name" type="text" class="form-control" placeholder="Your Name *" id="name" required>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <input name="email" type="email" class="form-control" placeholder="Your Email *" id="email" required>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <textarea name="message" class="form-control" placeholder="Your Message *" id="message" required></textarea>
+                            </div>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-12 text-center">
+                            <div id="success"></div>
+                            <button id= "send_email" type="submit" class="btn">Send Message</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+
+<p id="back-top">
+    <a href="#top"><i class="fa fa-angle-up"></i></a>
+</p>
+<footer>
+
+    <div class="container text-center">
+        <div class="footer-container row">
+            <div class="social col-md-4 col-sm-12">
+                <a href=""><img src="img/facebook.svg" alt="facebook link"></a>
+                <a href=""><img src="img/twitter.svg" alt="twitter link"></a>
+                <a href=""><img src="img/instagram.svg" alt="insta link"></a>
+            </div>
+
+            <div class="copy col-md-4 col-sm-6">
+                <p>&copy; Cindy Moody 2018</p>
+                <p>All rights reserved.</p>
+            </div>
+
+            <div class=" links col-md-4 col-sm-6">
+                    <p><a href="#">Privacy Policy</a></p>
+                    <p><a href="#">Terms Of Use</a></p>
+                    <p><a href="#">Site Designer</a></p>
+
+            </div>
+        </div>
+
+    </div>
+</footer>
+</div>
+
+<!-- Bootstrap core JavaScript
+    ================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/SmoothScroll.js"></script>
+<script src="js/theme-scripts.js"></script>
+<script src="js/imageData.js"></script>
+</body>
+</html>
