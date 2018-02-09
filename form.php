e67c1aa88bc5cd28620c07e2cc643415af01bce5
lee nolan <leenolan@lees-MacBook-Air.local>
2018-02-09 15:28:47 +0000

    sass refactor

--- form.php
+++ form.php
@@ -1,16 +1,19 @@
 <?php
 
+// this script requires autoload.php to use the SendGrid API
 require 'vendor/autoload.php';
 
-
+// check for form submission, if form submitted, trim and sanitize the inputs, assign them to variables
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $name = trim(filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING));
     $email = trim(filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL));
     $message = trim(filter_input(INPUT_POST, "message", FILTER_SANITIZE_SPECIAL_CHARS));
 
+    // build email body content
     $emailBody = "From: " . $name . "<br>" . "EMail: " . $email . "<br>" . "Message: " . $message;
 
+    // SendGrid settings and config
     $from = new SendGrid\Email("Cindy Moody Art Website","leenolan@icloud.com");
     $subject = "Cindy, you have a new artwork enquiry!";
     $to = new SendGrid\Email(null, "leenolan79@icloud.com");
@@ -26,6 +29,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo $response->headers();
     echo $response->body();
 
+    // redirects user to 'thanks' page after successful form submission
     header("Location: thanks.php");
 
 }
--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <!-- Custom styles -->
-    <link href="css/style.css" rel="stylesheet">
+    <link href="css/output.css" rel="stylesheet">
 </head>
 
 <body id="page-top">
